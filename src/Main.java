ffb0f2ad4c0f84d95d2b88b851450761b76dca02
lope36 <61106919+lope36@users.noreply.github.com>
2020-02-16 10:54:07 +0100

    rampamapam

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
+import java.time.LocalDate;
+import java.time.Period;
 
-public class Main extends Application {
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
+        System.out.print("urodzony 17.02.2002 - ");
+        if (majority(17,2,2002))
+            System.out.println("pełnoletni");
+        else
+            System.out.println("niepełnoletni");
+
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+        public static boolean majority (int day, int month, int year){
+            LocalDate dateOfBirth, now;
+            dateOfBirth = LocalDate.of(year,month,day);
+            now = LocalDate.now();
+            Period p = Period.between(dateOfBirth, now);
+            if (p.getYears()>= 18) return true;
+            return false;
+
+
+        }
+
+
 
     }
-}
+
